b1490a7b8d5c2709c94921855f47a1dc2d3ef12f
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-05-03 17:18:38 -0500

    works on headless and stop on N amount of post

--- index.js
+++ index.js
@@ -22,39 +22,39 @@ async function scrapeItems(
   page, // A reference to the Puppeteer page object
   extractItems, // A reference to the extractItems() function
   itemCount, // The number of TikTok videos to scrape
-  scrollDelay = 800, // The delay in milliseconds between page scrolls (default: 800)
-  scrollPosition = 10000, // The position to scroll to before stopping (default: 10000)
+  scrollDelay = 300, // The delay in milliseconds between page scrolls (default: 800)
 ) {
-  let items = []; // Initialize an empty array to hold the scraped data
+  let items = [];
   try {
-    let currentScrollPosition = 0;
-    // Scroll the page until either the desired number of items have been scraped or the end of the page has been reached
-    while (currentScrollPosition < scrollPosition || items.length < itemCount) {
-      await page.evaluate(`window.scrollBy(0, 5000)`); // Scroll down by 2000 pixels
+    let currentDivItemContainerCount = 0;
+    while (currentDivItemContainerCount < maxDivItemContainerCount || items.length < itemCount) {
+      await page.evaluate(`window.scrollBy(0, 2500)`); // Scroll down by 2500 pixels
       await page.waitForTimeout(scrollDelay); // Wait for the specified delay
-      await page.evaluate(`window.scrollBy(0, -2500)`); // Scroll up by 1000 pixels
+      await page.evaluate(`window.scrollBy(0, 2500)`); // Scroll down by another 2500 pixels
       await page.waitForTimeout(scrollDelay); // Wait for the specified delay
-      // Get the current scroll position and check if the end of the page has been reached
-      currentScrollPosition = await page.evaluate('window.scrollY + window.innerHeight');
-      if (currentScrollPosition >= scrollPosition) {
-        // If the end of the page has been reached, extract the data and add it to the array
-        items = await page.evaluate(extractItems);
+      await page.evaluate(`window.scrollBy(0, -2500)`); // Scroll up by 2500 pixels
+      await page.waitForTimeout(scrollDelay); // Wait for the specified delay
+      currentDivItemContainerCount = await page.$$eval('.tiktok-x6y88p-DivItemContainerV2', (divs) => divs.length);
+      console.log(`Current number of elements: ${currentDivItemContainerCount}`);
+      if (currentDivItemContainerCount >= maxDivItemContainerCount) {
+        // If the maximum number of .tiktok-x6y88p-DivItemContainerV2 elements have been loaded, exit the loop
+        break;
       }
     }
-  } catch (e) {
-    // Catch any errors that occur during scraping
-    console.error(e);
-  }
-  // Return the array of scraped data
+    // Extract the data after the loop exits, regardless of whether the maximum number of .tiktok-x6y88p-DivItemContainerV2 elements have been loaded
+    items = await page.evaluate(extractItems);
+  } catch (e) {}
   return items;
 }
+
+
 
 // Main function that runs the scraping script
 (async () => {
   // Launch a Puppeteer browser instance
   const browser = await puppeteer.launch({
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome', // Path to the Chrome executable
-    headless: false, // Whether to run the browser in headless mode (false = non-headless, true = headless)
+    headless: 'new' // Whether to run the browser in headless mode (false = non-headless, true = headless)
   });
   // Open a new page in the browser
   const page = await browser.newPage();
@@ -64,7 +64,7 @@ async function scrapeItems(
   await page.goto('https://www.tiktok.com/@dior', { waitUntil: 'networkidle2', timeout: 0 });
 
   // Wait for the video item containers to load on the page
-  await page.waitForSelector('.tiktok-x6y88p-DivItemContainerV2', { timeout: 60000 });
+  await page.waitForSelector('.tiktok-x6y88p-DivItemContainerV2', { timeout: 0 });
   
   // Scrape the desired number of TikTok video data from the page
   const items = await scrapeItems(page, extractItems, 150);
@@ -75,3 +75,6 @@ async function scrapeItems(
   // Close the Puppeteer browser instance
   await browser.close();
   })();
+
+  
+  
